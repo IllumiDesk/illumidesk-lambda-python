32a659fcf19e66789fb783b40f19e9641cfb082e
ao <alexandre.osin@gmail.com>
2023-04-24 18:46:28 +0300

    IL-3543 Update run_python_code handler

--- context/app.py
+++ context/app.py
@@ -1,4 +1,18 @@
+import io
 import json
+import sys
+
+
+class OutputCatcher(io.StringIO):
+    def __init__(self):
+        self.buffer = ""
+        super().__init__(self.buffer)
+
+    def write(self, msg):
+        self.buffer += msg
+
+    def flush(self):
+        pass
 
 
 def lambda_handler(event, context):
@@ -15,28 +29,37 @@ def lambda_handler(event, context):
     :returns: Any type of data. In case of integration with Amazon API Gateway,
     the response must comply with its format.
     """
-    code = event.get('code')
-    if code:
-        try:
-            output = {}
-            exec(code, {}, output)
-            return {
-                'statusCode': 200,
-                'body': json.dumps({
-                    'result': output
-                })
+    codes: list[dict] = event.get("codes")
+    if codes:
+        results = []
+        exec_globals = {}
+        exec_locals = {}
+
+        for code_block in codes:
+            output_catcher = OutputCatcher()
+            sys.stdout = output_catcher
+
+            result = {
+                "id": code_block["id"],
+                "outputs": [],
             }
-        except Exception as ex:
-            return {
-                'statusCode': 400,
-                'body': json.dumps({
-                    'error': str(ex)
-                })
-            }
-    else:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({
-                'error': 'No code provided'
-            })
+
+            try:
+                exec(code_block["code"], exec_globals, exec_locals)
+            except Exception as ex:
+                result["outputs"].append({"type": "error", "content": str(ex)})
+
+            sys.stdout = sys.__stdout__
+            output = output_catcher.buffer
+            if output:
+                result["outputs"].append({"type": "stdout", "content": output})
+
+            results.append(result)
+
+        response = {
+            "statusCode": 200,
+            "body": json.dumps({"results": results}),
         }
+        return response
+    else:
+        return {"statusCode": 400, "body": json.dumps({"error": "No code provided"})}
--- tests/lambda_function/test_lambda_with_arn.py
+++ tests/lambda_function/test_lambda_with_arn.py
@@ -4,7 +4,7 @@ import os
 import boto3
 
 
-def run_python_code(code: str) -> str:
+def run_python_code(codes: list[dict]) -> str:
     """
     Runs Python code on AWS Lambda.
 
@@ -15,31 +15,30 @@ def run_python_code(code: str) -> str:
         json: Result of the Python code execution as a json formatted string.
     """
     # Initialize the AWS Lambda client
-    lambda_client = boto3.client('lambda')
+    lambda_client = boto3.client("lambda")
 
     # Set up the payload with the python code to be executed on Lambda
-    payload = {
-        'code': code
-    }
+    payload = {"codes": codes}
 
     # Invoke the Lambda function with the payload
     response = lambda_client.invoke(
         # Specify the Lambda function ARN (Amazon Resource Name)
-        FunctionName=os.getenv('LAMBDA_FUNCTION_ARN'),
+        FunctionName=os.getenv("LAMBDA_FUNCTION_ARN"),
         # Set the invocation type to synchronous 'RequestResponse'
-        InvocationType='RequestResponse',
+        InvocationType="RequestResponse",
         # Convert the payload to a JSON-formatted string
-        Payload=json.dumps(payload)
+        Payload=json.dumps(payload),
     )
 
     # Parse the Lambda response payload and read the result
-    result = json.loads(response['Payload'].read())
+    result = json.loads(response["Payload"].read())
 
     # Return the result from the Lambda function
     return result
 
 
-if __name__ == '__main__':
-    TEST_CODE = 'a = 2 + 3; b = a * 4'
-    test_result = run_python_code(TEST_CODE)
+if __name__ == "__main__":
+    TEST_CODE = "a = 2 + 3; b = a * 4"
+    codes = [{"id": 1, "code": TEST_CODE}]
+    test_result = run_python_code(codes)
     print(test_result)
